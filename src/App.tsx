e3fe18fd857ad442dc3fcd5d933b9d3a17fd8f9f
tokaabdulhamied <tokaabdulhamied@gmail.com>
2023-09-28 22:07:27 +0300

    Add custom font 'Cairo'

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,24 @@ import { useTranslation } from "react-i18next"
 
 function App() {
   const { t } = useTranslation()
-  return <div className="App">{t("hello")}</div>
+  const { i18n } = useTranslation()
+  function changeLanguage(e: any) {
+    i18n.changeLanguage(e.target.value)
+  }
+  return (
+    <div className="App">
+      <nav>nav</nav>
+      <main>
+        <h1>title</h1>
+        <section>search input</section>
+      </main>
+      <main>
+        <section>stepper</section>
+        <section>table</section>
+        <aside>asid</aside>
+      </main>
+    </div>
+  )
 }
 
 export default App
