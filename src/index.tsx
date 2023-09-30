27409be134fda938ba4f0f96e2c17a19d0ee70cf
tokaabdulhamied <tokaabdulhamied@gmail.com>
2023-09-30 14:19:58 +0300

    change font family of  material theme

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,22 @@ import { I18nextProvider } from "react-i18next"
 import i18n from "i18n/config"
 import { Provider } from "react-redux"
 import store from "Store/store"
+import { ThemeProvider, createTheme } from "@mui/material"
+const theme = createTheme({
+  typography: {
+    fontFamily: "Cairo-Meduim",
+  },
+})
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
     </I18nextProvider>
   </React.StrictMode>
 )
